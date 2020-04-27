e9a4ff7ab16024b64224455a381008887de228cc
zahra <zahra.sheykhlar@gmail.com>
2020-04-27 08:29:16 +0430

    FIRST COMMIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+import { LatestRatesComponent } from './latest-rates/latest-rates.component';
+import { LastThirtyDaysComponent } from './last-thirty-days/last-thirty-days.component';
+import { TopFiveComponent } from './top-five/top-five.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import {MatIconModule} from '@angular/material/icon';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { IncreaseDecreaseComponent } from './increase-decrease/increase-decrease.component';
+import { TopIncreaseComponent } from './top-increase/top-increase.component';
+import { TopDecreaseComponent } from './top-decrease/top-decrease.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputUserDataFormComponent,
+    LatestRatesComponent,
+    LastThirtyDaysComponent,
+    TopFiveComponent,
+    IncreaseDecreaseComponent,
+    TopIncreaseComponent,
+    TopDecreaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule,
+    MatIconModule,
+    NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
